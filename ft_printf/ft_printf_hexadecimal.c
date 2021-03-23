da3f2a058f16c836fef0934e666941a73d1d1092
Anderson-ubuntu <ander_alvarenga@hotmail.com>
2021-03-23 18:00:44 -0300

    hexa

--- ft_printf/ft_printf_hexadecimal.c
+++ ft_printf/ft_printf_hexadecimal.c
@@ -6,7 +6,7 @@
 /*   By: aalcara- <aalcara-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/21 20:51:59 by aalcara-          #+#    #+#             */
-/*   Updated: 2021/03/23 17:55:44 by aalcara-         ###   ########.fr       */
+/*   Updated: 2021/03/23 18:00:35 by aalcara-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,6 +65,7 @@ static char		*itoa_hex(unsigned long int nbr, t_flags flags, int specifier)
 	int			aux_num;
 	char		*hexa_str;
 	char		*hexa_ret;
+	char		*free_hexa;
 
 	num_len = ft_hexa_len(nbr);
 	if (!(hexa_str = ft_calloc(sizeof(char), (num_len + 1))))
@@ -89,6 +90,8 @@ static char		*itoa_hex(unsigned long int nbr, t_flags flags, int specifier)
 		hexa_ret = ptr_hex(hexa_str, flags);
 	else
 		hexa_ret = precision_hex(hexa_str, flags);
+	if (nbr > 0)
+		free_hexa = ft_free(hexa_ret, ft_strlen(hexa_ret));
 	return (hexa_ret);
 }
 
