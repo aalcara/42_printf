74d3d3759c672a9dda62063841d00fef921e125c
Anderson-ubuntu <ander_alvarenga@hotmail.com>
2021-03-25 20:50:22 -0300

    undefined behavior corrections

--- main.c
+++ main.c
@@ -176,7 +176,7 @@ int main(void)
 	printf("\treturn :%d\n", printf("|%09s|", s_hidden));
 	printf("\treturn :%d\n", ft_printf("|%09s|", s_hidden));
 
-	printf("\n---- new2019_negstar_9 ----\n");
+	printf("\n15: ---- new2019_negstar_9 ----\n");
 	printf("\treturn :%d\n", printf("|%0*i|", -7, -54));
 	printf("\treturn :%d\n", ft_printf("|%0*i|", -7, -54));
 
